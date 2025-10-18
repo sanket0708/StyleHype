07871e8f06a882aeefff231bb9633753fdb2d42e
sanket0708 <sanketmudhulkar98@gmail.com>
2025-10-18 14:49:50 +0530

    Setup login and signup routes

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+//connect mongo
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to style hype");
+});
+
+//API ROUTES
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
